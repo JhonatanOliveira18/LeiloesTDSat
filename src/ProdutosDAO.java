26d4dacb5234b9b578bde59ce71a005d9d5f66fe
Jhonatan Oliveira <oliveirajhonatanjesus18@gmail.com>
2026-04-02 08:37:50 -0300

    feat: adição da funcionalidade salvar e mensagem de sucesso/insucesso da ação

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -25,7 +26,28 @@ public class ProdutosDAO {
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
+        
+        //Instrução SQL
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+        
+        try {
+            //Controlando e executando a Instrução SQL
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());//Alocando dado na primeira interrogação que equivale ao nome
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            //Enviando para o MySQL
+            prep.executeUpdate();
+            
+            //Mensagem de confirmação
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso.");
+        } catch (SQLException ex) { //Coleta os possíveis erros e exibe através do getMessage()
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + ex.getMessage());
+        }finally{
+            
+        }    
         
         
     }
